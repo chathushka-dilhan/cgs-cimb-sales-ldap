6bd1b78958725358250b9e7e79073e5ed5e7dc26
Dilhan CHATHUSHKA <dilhan.hettipathirana@staizen.com>
2021-05-16 23:32:24 +0800

    Updated express app for lambda with serverless deployement

---
+++ app.js
@@ -0,0 +1,71 @@
+var settings = require('./config/config.json');
+
+var bodyParser = require('body-parser');
+var jwt = require('jwt-simple');
+var moment = require('moment');
+var LdapAuth = require('ldapauth-fork');
+var Promise  = require('promise');
+
+const serverless = require('serverless-http');
+const express = require('express');
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(require('cors')());
+
+var auth = new LdapAuth(settings.ldap);
+
+app.set('jwtTokenSecret', settings.jwt.secret);
+
+var authenticate = function (username, password) {
+	return new Promise(function (resolve, reject) {
+		auth.authenticate(username, password, function (err, user) {
+			if(err)
+				reject(err);
+			else if (!user)
+				reject();
+			else
+				resolve(user);
+		});
+	});
+};
+
+app.post('/auth', function (req, res) {
+	if(req.body.username && req.body.password) {
+		authenticate(req.body.username, req.body.password)
+			.then(function(user) {
+				var expires = parseInt(moment().add(2, 'days').format("X"));
+				var token = jwt.encode({
+					exp: expires,
+					user_name: user.uid,
+					full_name: user.cn,
+					mail: user.mail
+				}, app.get('jwtTokenSecret'));
+
+				res.json({token: token, full_name: user.cn});
+			})
+			.catch(function (err) {
+				console.log(err);
+
+				if (err.name === 'InvalidCredentialsError' || (typeof err === 'string' && err.match(/no such user/i)) ) {
+					res.status(401).send({ error: 'Wrong user or password'});
+				} else {
+					res.status(500).send({ error: 'Unexpected Error'});
+					auth = new LdapAuth(settings.ldap);
+				}
+
+			});
+		} else {
+			res.status(400).send({error: 'No username or password supplied'});
+		}
+});
+
+/*var port = (process.env.PORT || 3000);
+app.listen(port, function() {
+	//console.log('Listening on port: ' + port);
+
+	if (typeof settings.ldap.reconnect === 'undefined' || settings.ldap.reconnect === null || settings.ldap.reconnect === false) {
+		console.warn('WARN: This service may become unresponsive when ldap reconnect is not configured.')
+	}
+});*/
+module.exports.handler = serverless(app);
