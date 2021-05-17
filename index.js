2ca8073019e5823c1770c399dd49a15c583000eb
Dilhan CHATHUSHKA <dilhan.hettipathirana@staizen.com>
2021-05-17 13:58:12 +0800

    Secured express app

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const express = require('express');
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.disable("x-powered-by");
 
 var auth = new LdapAuth(settings.ldap);
 
